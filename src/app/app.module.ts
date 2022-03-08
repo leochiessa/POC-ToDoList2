d8995ee6d44aee4ed01970d20a5ac30c0fe0c531
leo <leonardochiessa@gmail.com>
2022-03-08 17:23:01 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
